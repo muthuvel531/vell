8ad60c923488181cfcfe426bec5ed051fa1a695e
muthuvel531 <muthuvelumamaheswari62@gmail.com>
2026-04-12 16:18:13 +0530

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,30 +1,30 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  
-  // basePath: '/react', // Uncomment if the repository name is 'react'
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-}
-
-export default nextConfig
-
 // /** @type {import('next').NextConfig} */
 // const nextConfig = {
-//   output: 'export',
-
+  
+//   // basePath: '/react', // Uncomment if the repository name is 'react'
 //   typescript: {
 //     ignoreBuildErrors: true,
 //   },
-
 //   images: {
 //     unoptimized: true,
 //   },
-
-//   basePath: '/jope',
 // }
 
 // export default nextConfig
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+
+  images: {
+    unoptimized: true,
+  },
+
+  basePath: '/jope',
+}
+
+export default nextConfig
